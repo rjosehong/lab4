a04dfa50aa0e92407e2899049042653cb1022c3b
rodrigo <rodrigojosehong@gmail.com>
2025-11-09 19:40:17 -0600

    Delete Edit.java

--- Edit.java
+++
@@ -1,8 +0,0 @@
-public class Edit extends Usuario implements Editor{
-    public Edit(String user, String password){
-        super(user, password);
-    }
-    public void Editor(){
-        System.out.println("Bievenido editor");
-    }
-}
